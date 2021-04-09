d1602604c52fbb286b3db4998a33badc7a932f70
lenovo <abdusatarovfaruh3@gmail.com>
2021-04-09 16:08:09 +0300

    init

---
+++ app/src/main/java/com/abdusatarov/navigationview/DrawerActivity.java
@@ -0,0 +1,46 @@
+package com.abdusatarov.navigationview;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.drawerlayout.widget.DrawerLayout;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.LinearLayout;
+
+import com.google.android.material.navigation.NavigationView;
+
+public class DrawerActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_drawer);
+
+        NavigationView navigationView = findViewById(R.id.navigation_view);
+        DrawerLayout drawerLayout = findViewById(R.id.drawer_layout);
+        LinearLayout linearLayout = findViewById(R.id.linear);
+
+        drawerLayout.addDrawerListener(new DrawerLayout.DrawerListener() {
+            @Override
+            public void onDrawerSlide(@NonNull View drawerView, float slideOffset) {
+                linearLayout.setTranslationX( (slideOffset * navigationView.getWidth()) / 2 );
+            }
+
+            @Override
+            public void onDrawerOpened(@NonNull View drawerView) {
+
+            }
+
+            @Override
+            public void onDrawerClosed(@NonNull View drawerView) {
+
+            }
+
+            @Override
+            public void onDrawerStateChanged(int newState) {
+
+            }
+        });
+    }
+}
